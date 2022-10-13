8d1f5988e662874074620a67e872f681b51e1475
haile7516 <haile7516@gmail.com>
2022-10-13 10:08:36 -0700

    Task Qa

--- 0x00-hello_world/101-quate.c
+++ 0x00-hello_world/101-quate.c
@@ -1,20 +1,32 @@
-#include <stdio.h>
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
+
 
 #include <unistd.h>
 
+
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ * Return: Always 1.
  */
 
 int main(void)
 
 {
 
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		write(2,
 
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 
-return (1);
+					      	      59);
+
+
+
+			return (1);
 
 }
