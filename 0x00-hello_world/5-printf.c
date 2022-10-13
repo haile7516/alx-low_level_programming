befe13766ac0ef33aaef3c6d89020405b8f64dd2
haile7516 <haile7516@gmail.com>
2022-10-13 06:07:33 -0700

    Task Hel5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,8 @@
 
 /**  
  * main - Print a string
- *
  * Description: use printf
- *
  * Return: 0
- *
  */
 
 
@@ -17,8 +14,8 @@ int main(void)
 
 {
 
-		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+	return (0);
 
 }
