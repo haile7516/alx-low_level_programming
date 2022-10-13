8149eba5841f58b794d1ee5216a904049d4936a0
haile7516 <haile7516@gmail.com>
2022-10-13 09:49:21 -0700

    Task Quatasion

--- 0x00-hello_world/101-quate.c
+++ 0x00-hello_world/101-quate.c
@@ -1,41 +1,18 @@
 #include <stdio.h>
 
-
+#include <unistd.h>
 
 /**
- * main - prints to string
- * Description: Prints "and that piece of art is useful.." without puts
- * Return: 1
+ * main - Entry point
+ * Return:1
  */
-
-
 
 int main(void)
 
 {
 
-      char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 
-      long l = 59;
-
-      long fd = 1;
-
-      long syscall = 1;
-
-      long ret = 0;
-
-       __asm__ ("syscall"
-
-      : "=a" (ret)
-
-      : "a" (syscall),
-
-      "D" (fd),
-
-      "S" (s),
-
-      "d" (l));
-
-      return (1);
+return (1);
 
 }
