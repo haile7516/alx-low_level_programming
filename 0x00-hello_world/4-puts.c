8665901de17d844d9e271e2165e880e67a508ee4
haile7516 <haile7516@gmail.com>
2022-10-13 05:52:59 -0700

    Task Hello4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 
 
-
-/**
- *
- *  *main - Entry point
- *
- *  Description: prints "\"Programming is like building a multilingual puzzle"
- *
- *    * Return: 0
- *
- *     */
-
-
-
 int main(void)
 
 {
