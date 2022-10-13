0893d5c57f196da9ad3d8beddda17d64235f1fcc
haile7516 <haile7516@gmail.com>
2022-10-13 09:59:00 -0700

    Task Qutasion

--- 0x00-hello_world/101-quate.c
+++ 0x00-hello_world/101-quate.c
@@ -4,14 +4,16 @@
 
 /**
  * main - Entry point
- * Return:1
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
 
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
 return (1);
 
