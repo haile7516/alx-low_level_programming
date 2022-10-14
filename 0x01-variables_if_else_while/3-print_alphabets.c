e5c26e3059df582fb0c235f05f1af9c88a5c2f31
haile7516 <haile7516@gmail.com>
2022-10-14 08:11:30 -0700

    Task alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -18,26 +18,29 @@ int main(void)
 
 {
 
-       char letter;
+	char letter;
 
 
 
-       for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 
-	       putchar(letter);
+		putchar(letter);
 
 
 
 	for (letter = 'A'; letter <= 'Z'; letter++)
 
-	        putchar(letter);
+		putchar(letter);
 
 
 
-	  putchar('\n');
+	putchar('\n');
 
 
 
 	return (0);
 
 }
+
+
+
