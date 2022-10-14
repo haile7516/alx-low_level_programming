5c55607472ffa31941a7c4dab99f8525cb3314d9
haile7516 <haile7516@gmail.com>
2022-10-14 08:17:59 -0700

    Task print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -18,26 +18,26 @@ int main(void)
 
 {
 
-        char letter;
+	char letter;
 
 
 
-	 for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 
-	 {
+	{
 
 		if (letter != 'e' && letter != 'q')
 
-		
-			putchar(letter);
+												putchar(letter);
 
-	   }
-
-
-	   putchar('\n');
+	}
 
 
 
-	   return (0);
+	putchar('\n');
+
+
+
+	return (0);
 
 }
