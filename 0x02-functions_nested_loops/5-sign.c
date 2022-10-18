465c1e198f7f02509740a70871cf86938815773d
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2022-10-18 12:33:42 -0700

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,11 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * print_sign - returns 1 if n is +, - if n < 0 0 otherwise
- * @n: integer to be tested
- * Return: 1, -1 or 0
+ * print_sign - prints the sign of a number
+ *@n: number being tested
+ * Return: Always 0.
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -16,9 +18,10 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
+	return (0);
 }
