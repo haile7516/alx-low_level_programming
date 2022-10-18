25de10f41b6ce40b065aab8137d83872425b2df3
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2022-10-18 12:53:11 -0700

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,108 +1,32 @@
 #include <stdio.h>
+#include "main.h"
 
-int powB(int base, int power)
-{
-	int i, prod = 1;
-	for (i = 0; i < power; i++)
-	{
-		prod = prod * base;
-	}
-	return prod;
-}
-
-int numLength(int num)
-{
-	int length = 0;
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-
-	return length;
-}
-void putnchar(int num)
-{
-	if (num == 0){
-		putchar(48);
-		return ;
-	}
-	int length = numLength(num);
-	int j = length - 1;
-	while (j >= 0)
-	{
-		//printf("tmp-->%i , char-->%i\n",tmp,tmp/powB(10,j)+48);
-		if (num % powB(10,j) == 0 && j != 0){
-			putchar(48 + num / powB(10, j));
-
-			int k;
-			for (k=j;k>0;k--)
-			{
-				putchar(48);
-
-			}
-			j=-1;
-		}
-		else
-		{
-			int digit1 = num / powB(10, j);
-			putchar(digit1 + 48);
-
-			int tmp2 = num;
-			num -= powB(10, j) * digit1 ;
-			if (numLength(tmp2) - numLength(num) == 2)
-			{
-				putchar(48);
-				j--;
-			}
-			j--;
-		}
-	}
-
-}
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
 void print_to_98(int n)
 {
-
-	int i,j;
-
-	if (n <= 98)
-	{
-		for (i = n; i < 98; i++)
-		{
-			if (i < 0)
-			{
-				putchar('-');
-				putnchar(-1 * i);
-			}else
-			{
-				putnchar(i);
-			}
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	else
-	{
-		for (i = n; i > 98; i--)
-		{
-			putnchar(i);
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar(48+9);
-	putchar(48+8);
-	putchar('\n');
-}
-
-int main(void)
+int i, j;
+if (n <= 98)
 {
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-	return 0;
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
+else if (i == 98)
+printf("%d\n", i);
+}
+}
+else if (n >= 98)
+{
+for (j = n; j >= 98; j--)
+{
+if (j != 98)
+printf("%d, ", j);
+else if (j == 98)
+printf("%d\n", j);
+}
+}
 }
