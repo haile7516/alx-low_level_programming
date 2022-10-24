f9e0d061011adc1da0646d56626f41814a1f0977
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2022-10-24 08:26:02 -0700

    Update 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src, including \0.
+ *@src: pointer.
+ *@dest: pointer.
+ *Return: the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, length;
+
+	for (length = 0; src[length] != '\0'; length++)
+	{
+	}
+
+	for (i = 0; i <= length ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+Footer
+© 2022 GitHub, Inc.
