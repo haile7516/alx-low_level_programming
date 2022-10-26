586f680810c806b39d5a13b30ce0b9bdeae8cfff
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2022-10-26 12:21:19 -0700

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -8,6 +7,7 @@
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
  */
+
 void print_buffer(char *b, int size)
 {
 	int byte, index;
@@ -20,35 +20,25 @@ void print_buffer(char *b, int size)
 		{
 			if ((index + byte) >= size)
 				printf("  ");
-
 			else
 				printf("%02x", *(b + index + byte));
-
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
 		}
-
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
 				break;
-
 			else if (*(b + index + byte) >= 31 &&
 				 *(b + index + byte) <= 126)
 				printf("%c", *(b + index + byte));
-
 			else
 				printf(".");
 		}
-
 		if (byte >= size)
 			continue;
-
 		printf("\n");
 	}
-
 	if (size <= 0)
 		printf("\n");
 }
-Footer
-© 2022 GitHub, Inc.
