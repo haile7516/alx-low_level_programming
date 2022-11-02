224270c49311a59d35a94a99cdd929010f36f770
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2022-11-02 10:35:20 -0700

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
  * Return: No.
  */
 
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(char c);
+		_puts_recursion(char *s);
 	}
 	else
 	{
