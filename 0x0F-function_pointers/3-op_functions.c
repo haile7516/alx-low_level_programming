d93e037e0b76c74bb6403a81daab83e957a0fa42
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2022-11-16 12:06:15 -0800

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,61 +1,75 @@
 #include "3-calc.h"
-
 #include <stdlib.h>
-
-#include <string.h>
-
-
+#include <stdio.h>
 
 /**
+ * op_add - ...
+ * @a: ...
+ * @b: ...
  *
- *  * get_op_func - ...
- *
- *   * @s: ...
- *
- *    *
- *
- *     * Return: ...
- *
- *      */
-
-int (*get_op_func(char *s))(int, int)
-
+ * Return: ...
+ */
+int op_add(int a, int b)
 {
+	return (a + b);
+}
 
-		op_t ops[] = {
+/**
+ * op_sub - ...
+ * @a: ...
+ * @b: ...
+ *
+ * Return: ...
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
-				{ "+", op_add },
+/**
+ * op_mul - ...
+ * @a: ...
+ * @b: ...
+ *
+ *  Return: ...
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
 
-					{ "-", op_sub },
+/**
+ * op_div - ...
+ * @a: ...
+ * @b: ...
+ *
+ *  Return: ...
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-						{ "*", op_mul },
+	return (a / b);
+}
 
-							{ "/", op_div }
+/**
+ * op_mod - ...
+ * @a: ...
+ * @b: ...
+ *
+ * Return: ...
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-					{ "%", op_mod },
-
-							{ NULL, NULL }
-
-						};
-
-			int i = 0;
-
-
-
-				while (i < 5)
-
-						{
-
-									if (strcmp(s, ops[i].op) == 0)
-
-													return (ops[i].f);	
-
-											i++;
-
-												}
-
-
-
-					return (0);
-
+	return (a % b);
 }
